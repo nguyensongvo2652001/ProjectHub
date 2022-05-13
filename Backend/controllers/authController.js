6d2e958e700e8d05e11dc3e9017433c6785052eb
unknown <nguyensongvo2652001@gmail.com>
2022-05-13 12:41:44 +0700

    Added AppError and catchAsync utilities

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -16,7 +16,6 @@ const createAndSendToken = async (args) => {
       Number(process.env.DAY_SECONDS) *
       1000,
     httpOnly: true,
-    // Encrypt the cookie only if the request is secured or if the host is https
     secure: req.secure || req.header("x-forwarded-proto") === "https",
   });
 
@@ -30,15 +29,14 @@ const createAndSendToken = async (args) => {
 };
 
 const login = async (req, res, next) => {
-  //1. Get the email and password
   const { email, password } = req.body;
-  //2. Check if email or password is empty
+
   if (!email || !password) {
     //Handle error here
   }
-  //3. Find the account with the specified email
+
   const user = await User.findOne({ email }).select("+password");
-  //4. Verify password
+
   const correctCredentials =
     user && (await user.verifyPassword(password, user.password));
 
@@ -46,22 +44,19 @@ const login = async (req, res, next) => {
     //Handle error here
   }
 
-  //5. Create and send back JWT if successful
   createAndSendToken({ req, res, user, statusCode: 200 });
 };
 
 const signUp = async (req, res, next) => {
-  //1. Sanitize user input
+  //Sanitize user input
   const userInput = filterObject(req.body, {
     whiteList: ["email", "password", "jobTitle", "name"],
   });
 
-  //2. Create the user
   const user = await User.create(userInput);
 
-  //3. Hash the password (using user document middleware) BEFORE saving the user to the database
+  //Hash the password (using user document middleware) BEFORE saving the user to the database
 
-  //4. Create JWT and send it back to the client.
   createAndSendToken({ user, req, res, statusCode: 201 });
 };
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-/* Configurate environment variables */
-/***************************************/
+// Configurate environment variables
 const dotenv = require("dotenv");
 dotenv.config({ path: "./env/config.env" });
-/***************************************/
 
 const app = require("./app");
 
-/* Connect to database */
-/***************************************/
+// Connect to database
 let databaseString = process.env.LOCAL_DB_STRING.replace(
   /<username>/,
   process.env.DB_USERNAME
@@ -18,7 +15,6 @@ databaseString = databaseString.replace(/<password>/, process.env.DB_PASSWORD);
 mongoose.connect(databaseString, () => {
   console.log("DATABASE CONNECTED");
 });
-/***************************************/
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
