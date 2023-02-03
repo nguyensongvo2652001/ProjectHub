350ae8675423d6eced749eea75c7a87082f2b52c
unknown <nguyensongvo2652001@gmail.com>
2023-02-03 18:00:55 +0700

    Connect to database

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,19 @@ dotenv.config({ path: "./env/main.env" });
 
 const app = require("./app");
 
+mongoose.set("strictQuery", true);
+
+const rawDatabaseURI = process.env.RAW_DATABASE_URI;
+const databasePassword = process.env.DATABASE_PASSWORD;
+const databaseURI = rawDatabaseURI.replace("<password>", databasePassword);
+mongoose.connect(databaseURI, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Connected to database successfully");
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
